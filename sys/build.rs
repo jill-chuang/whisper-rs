2ba60032d915e48ded4eaea5aa9e87bcda6d107f
jill-chuang <chichuang0813@gmail.com>
2025-06-26 15:19:00 +0800

    feat: add Vulkan-Hpp to improve inference speed

--- sys/build.rs
+++ sys/build.rs
@@ -3,53 +3,75 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let whisper_cpp_path = PathBuf::from("whisper.cpp");
-    println!("cargo:rerun-if-changed={}", whisper_cpp_path.to_str().unwrap());
+    let target = env::var("TARGET").unwrap_or_else(|_| env::var("CARGO_CFG_TARGET").unwrap());
 
-    let mut cmake_config = cmake::Config::new(whisper_cpp_path.clone());
-    cmake_config.define("BUILD_SHARED_LIBS", "OFF");
-    
-    let target = env::var("TARGET").unwrap();
     if target == "aarch64-linux-android" {
-        println!("cargo:warning=--- Enabling NEON for aarch64-linux-android ---");
-        cmake_config.define("WHISPER_NEON", "ON");
-        cmake_config.define("WHISPER_OPENMP", "ON");
-        cmake_config.define("GGML_VULKAN", "ON");
+        println!("cargo:warning=--- [LINK-ONLY MODE] Linking PRE-BUILT libraries for Android ---");
 
         let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-        let whisper_rs_root = manifest_dir.parent().unwrap(); 
-        let zlib_lib_dir = whisper_rs_root.join("third_party").join("zlib_android").join("lib");
+        let whisper_rs_root = manifest_dir.parent().unwrap();
+
+        let whisper_build_path = whisper_rs_root.join("sys/whisper.cpp/build-android-arm64");
+        let openblas_lib_path = whisper_rs_root.join("third_party/openblas_android/lib");
+
+        let zlib_lib_path = whisper_rs_root.join("third_party/zlib_android/lib");
+
+        println!("cargo:rustc-link-search=native={}", whisper_build_path.display());
+        println!("cargo:rustc-link-search=native={}", whisper_build_path.join("lib").display());
+        println!("cargo:rustc-link-search=native={}", whisper_build_path.join("src").display());
+        println!("cargo:rustc-link-search=native={}", whisper_build_path.join("ggml/src").display());
+        println!("cargo:rustc-link-search=native={}", whisper_build_path.join("ggml/src/ggml-blas").display());
+        println!("cargo:rustc-link-search=native={}", whisper_build_path.join("ggml/src/ggml-vulkan").display());
+        println!("cargo:rustc-link-search=native={}", openblas_lib_path.display());
+        println!("cargo:rustc-link-search=native={}", zlib_lib_path.display());
         
-        println!("cargo:rustc-link-search=native={}", zlib_lib_dir.display());
+        println!("cargo:rustc-link-lib=static=whisper");
+        println!("cargo:rustc-link-lib=static=ggml-vulkan");
+        println!("cargo:rustc-link-lib=static=ggml-blas");
+        println!("cargo:rustc-link-lib=static=openblas");
+        println!("cargo:rustc-link-lib=static=ggml"); 
+        println!("cargo:rustc-link-lib=static=ggml-cpu");
+        println!("cargo:rustc-link-lib=static=ggml-base");
         println!("cargo:rustc-link-lib=static=z");
+        
+        println!("cargo:rustc-link-lib=dylib=vulkan");
+        println!("cargo:rustc-link-lib=dylib=omp");
+        println!("cargo:rustc-link-lib=log");
+        println!("cargo:rustc-link-lib=c++_shared");
+
+    } else {
+        panic!("This build script is currently configured for Android manual link mode only.");
     }
 
-    let dst = cmake_config.build();
-    
-    println!("cargo:rustc-link-search=native={}", dst.join("lib").display());
-    println!("cargo:rustc-link-search=native={}", dst.join("lib64").display());
-    println!("cargo:rustc-link-search=native={}", dst.join("src").display());
-    println!("cargo:rustc-link-search=native={}", dst.join("ggml").join("src").display());
-    
-    println!("cargo:rustc-link-lib=static=ggml-base");
-    println!("cargo:rustc-link-lib=static=ggml-cpu");
-    println!("cargo:rustc-link-lib=static=ggml");
-    println!("cargo:rustc-link-lib=static=whisper");
-
+    let whisper_cpp_path = PathBuf::from("whisper.cpp");
     let header_path = whisper_cpp_path.join("include").join("whisper.h");
     println!("cargo:rerun-if-changed={}", header_path.to_str().unwrap());
-    let ndk_sysroot = format!("{}/toolchains/llvm/prebuilt/darwin-x86_64/sysroot", 
-                              env::var("ANDROID_NDK").expect("ANDROID_NDK is not set"));
-    let bindings = bindgen::Builder::default()
+
+    let mut builder = bindgen::Builder::default()
         .header(header_path.to_str().unwrap())
         .clang_arg("-I").clang_arg(whisper_cpp_path.join("include").to_str().unwrap())
         .clang_arg("-I").clang_arg(whisper_cpp_path.join("ggml").join("include").to_str().unwrap())
-        .clang_arg("-I").clang_arg(whisper_cpp_path.join("ggml").join("src").to_str().unwrap())
-        .clang_arg("--target=aarch64-linux-android")
-        .clang_arg(format!("--sysroot={}", ndk_sysroot))
         .allowlist_function("whisper_.*").allowlist_type("whisper_.*").allowlist_var("WHISPER_.*")
         .allowlist_function("ggml_.*").allowlist_type("ggml_.*")
-        .use_core()
-        .generate().expect("Unable to generate bindings");
+        .use_core();
+    
+    let target = env::var("TARGET").unwrap_or_else(|_| env::var("CARGO_CFG_TARGET").unwrap());
+    if target.contains("android") {
+        println!("cargo:warning=--- [BINDGEN_CONFIG] Configuring bindgen for Android target on Apple Silicon (using x86_64 toolchain) ---",);
+        
+        let ndk_path = env::var("ANDROID_NDK").expect("ANDROID_NDK is not set");
+        let api_level = 31; 
+        
+        let host_arch = "darwin-x86_64";
+        
+        let toolchain_path = format!("{}/toolchains/llvm/prebuilt/{}", &ndk_path, host_arch);
+        let sysroot = format!("{}/sysroot", &toolchain_path);
+        
+        builder = builder
+            .clang_arg(format!("--target={}{}", target, api_level))
+            .clang_arg(format!("--sysroot={}", sysroot));
+    }
+        
+    let bindings = builder.generate().expect("Unable to generate bindings");
     bindings.write_to_file(PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs")).expect("Couldn't write bindings!");
 }
